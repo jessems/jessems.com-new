64daaec2ee77c322b1df11932bba0e119f68663f
Jesse Szepieniec <jessems@gmail.com>
2023-07-23 21:30:45 +0200

    Fixed gen-rss

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,38 +1,54 @@
-const { promises: fs } = require('fs')
-const path = require('path')
-const RSS = require('rss')
-const matter = require('gray-matter')
+const { promises: fs } = require('fs');
+const path = require('path');
+const RSS = require('rss');
+const matter = require('gray-matter');
 
-async function generate() {
-  const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
-  })
-
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
-
-  await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return
-
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
-      )
-      const frontmatter = matter(content)
-
-      feed.item({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author,
-      })
-    })
-  )
-
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+// New function to check if a path is a file
+async function isFile(path) {
+	try {
+		const stat = await fs.stat(path);
+		return stat.isFile();
+	} catch (err) {
+		console.error(`Error checking if path is a file: ${path}`, err);
+		return false;
+	}
 }
 
-generate()
+async function generate() {
+	const feed = new RSS({
+		title: 'Your Name',
+		site_url: 'https://yoursite.com',
+		feed_url: 'https://yoursite.com/feed.xml'
+	});
+
+	const posts = await fs.readdir(
+		path.join(__dirname, '..', 'pages', 'posts')
+	);
+
+	await Promise.all(
+		posts.map(async name => {
+			const postPath = path.join(__dirname, '..', 'pages', 'posts', name);
+
+			// If the path is not a file, return and skip it
+			if (name.startsWith('index.') || !(await isFile(postPath))) {
+				return;
+			}
+
+			const content = await fs.readFile(postPath);
+			const frontmatter = matter(content);
+
+			feed.item({
+				title: frontmatter.data.title,
+				url: '/posts/' + name.replace(/\.mdx?/, ''),
+				date: frontmatter.data.date,
+				description: frontmatter.data.description,
+				categories: frontmatter.data.tag.split(', '),
+				author: frontmatter.data.author
+			});
+		})
+	);
+
+	await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
+}
+
+generate();
